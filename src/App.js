5cc7195aa54b33daba8ecf03d94172a8fda5fdfd
Yigit Dincsoy <w0471994@campus.nscc.ca>
2023-03-05 11:09:02 -0400

    well that is a a big update

--- src/App.js
+++ src/App.js
@@ -3,43 +3,27 @@ import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import Navigation from './components/Navigation'
 import Header from './components/Header'
 import Home from './components/Home'
-import About from './components/About'
+import Resume from './components/Resume'
+import Clicker from './components/Clicker/Clicker'
 import Minigame from './components/Minigame'
-import Countries from './components/Countries'
-import CountryDetails from './components/CountryDetails'
-import Admin from './components/Admin'
-import Login from './components/Auth/Login'
-import Logout from './components/Auth/Logout'
-import Signup from './components/Auth/Signup'
-import {AuthProvider} from './components/Auth/AuthProvider'
-import ProtectedRoutes from './components/Auth/ProtectedRoutes'
+import Nopage from './components/Nopage'
 
 function App() {
   return (
     <div className="App">
     <BrowserRouter>
-        <AuthProvider>
           <Header/>
           <Navigation/>
-
+          
+          //
           <Routes>
+            <Route path="*" element={<Nopage/>}/>
             <Route path="/" element={<Home/>}/>
-            <Route path="/about" element={<About/>}/>
+            <Route path="/resume" element={<Resume/>}/>
             <Route path="/minigame" element={<Minigame/>}/>
-            <Route path="/unicorn" element={<About/>}/>
-            <Route path="/countries" element={<Countries/>}>
-              <Route path="details/:countryname" element={<CountryDetails/>}/>
-              <Route path="hellokitty" element={<About/>}/>
-             </Route> 
-            <Route path="/admin" element={<ProtectedRoutes/>}>
-              <Route path="" element={<Admin/>}/>
-            </Route>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/signup" element={<Signup/>}/>
-            <Route path="/logout" element={<Logout/>}/>
+            <Route path="/clicker" element={<Clicker/>}/>   
           </Routes>
         
-        </AuthProvider>
       </BrowserRouter>
     </div>
   );
